563ed790d9fb5c0926b98991748883571b356d90
Huria Harshita <huriahar@teach.cs.toronto.edu>
2018-02-26 03:05:03 -0500

    Adding implementation for binary_ne_grid

--- assignment2/A2/kenken_csp.py
+++ assignment2/A2/kenken_csp.py
@@ -29,9 +29,57 @@ The grid-only models do not need to encode the cage constraints.
 
 '''
 
+from cspbase import *
+import itertools
+
 def binary_ne_grid(kenken_grid):
-    # TODO! IMPLEMENT THIS!
-    pass
+    N = kenken_grid[0][0]
+    # Generate the domain
+    domain = list(range(1, N+1))
+
+    # Generate a list of variables
+    variables = []
+    for i in range(1, N+1):
+        var_row = []
+        for j in range(1, N+1):
+            var_row.append(Variable('Var{}{}'.format(i, j), domain))
+        variables.append(var_row)
+
+    constraints = []
+    # Add row constraints i.e. 11 != 12 11 != 13 12 != 13 for row1
+    for i in range(1, N+1):
+        for j in range(1, N+1):
+            for k in range(2, N+1):
+                if j < k:
+                    constraint = Constraint("C(Var{}{},Var{}{})".format(i, j, i, k), [variables[i-1][j-1], variables[i-1][k-1]])
+                    constraints.append(constraint)
+
+
+    # Add col constraints i.e. 11 != 21 11 != 31 21 != 31
+    for i in range(1, N+1):
+        for j in range(1, N+1):
+            for k in range(2, N+1):
+                if i < k:
+                    constraint = Constraint("C(Var{}{},Var{}{})".format(i, j, k, j), [variables[i-1][j-1], variables[k-1][j-1]])
+                    constraints.append(constraint)
+
+    # Add satisfying tuples
+    for con in constraints:
+        satisfying_tuples = []
+        for tup in itertools.product(domain, domain):
+            if tup[0] != tup[1]:
+                satisfying_tuples.append(tup)
+        con.add_satisfying_tuples(satisfying_tuples)
+
+    variables = list(itertools.chain(*variables))
+
+    # Create CSP
+    csp = CSP("binaryNEKenKen", variables)
+    for con in constraints:
+        csp.add_constraint(con)
+
+    return csp, variables
+
 
 def nary_ad_grid(kenken_grid):
     # TODO! IMPLEMENT THIS!
