894bd3306599d9ac6964c17847a41d4b8e974a14
Huria Harshita <huriahar@teach.cs.toronto.edu>
2018-02-27 03:59:14 -0500

    Adding initial implementation for kenken csp model - not working

--- assignment2/A2/kenken_csp.py
+++ assignment2/A2/kenken_csp.py
@@ -29,8 +29,9 @@ The grid-only models do not need to encode the cage constraints.
 
 '''
 
+from operator import mul
 from cspbase import *
-import itertools
+import itertools, functools
 
 def binary_ne_grid(kenken_grid):
     N = kenken_grid[0][0]
@@ -46,12 +47,20 @@ def binary_ne_grid(kenken_grid):
         variables.append(var_row)
 
     constraints = []
+
+    # Add satisfying tuples
+    satisfying_tuples = []
+    for tup in itertools.product(domain, domain):
+        if tup[0] != tup[1]:
+            satisfying_tuples.append(tup)
+
     # Add row constraints Ex. 11 != 12 11 != 13 12 != 13 for row1
     for i in range(1, N+1):
         for j in range(1, N+1):
             for k in range(2, N+1):
                 if j < k:
                     constraint = Constraint("C(Var{}{},Var{}{})".format(i, j, i, k), [variables[i-1][j-1], variables[i-1][k-1]])
+                    constraint.add_satisfying_tuples(satisfying_tuples)
                     constraints.append(constraint)
 
 
@@ -61,15 +70,8 @@ def binary_ne_grid(kenken_grid):
             for k in range(2, N+1):
                 if i < k:
                     constraint = Constraint("C(Var{}{},Var{}{})".format(i, j, k, j), [variables[i-1][j-1], variables[k-1][j-1]])
+                    constraint.add_satisfying_tuples(satisfying_tuples)
                     constraints.append(constraint)
-
-    # Add satisfying tuples
-    for con in constraints:
-        satisfying_tuples = []
-        for tup in itertools.product(domain, domain):
-            if tup[0] != tup[1]:
-                satisfying_tuples.append(tup)
-        con.add_satisfying_tuples(satisfying_tuples)
 
     # Flatten the 2-D variables matrix to a list
     variables = list(itertools.chain(*variables))
@@ -96,22 +98,23 @@ def nary_ad_grid(kenken_grid):
         variables.append(var_row)
 
     constraints = []
+
+    # Add satisfying tuples
+    satisfying_tuples = []
+    for tup in itertools.permutations(domain, N):
+        satisfying_tuples.append(tup)
+
     # Add row constraints i.e. 11 != 12 != 13
     for row in range(1, N+1):
         constraint = Constraint("Row{}Con".format(row), variables[row-1])
+        constraint.add_satisfying_tuples(satisfying_tuples)
         constraints.append(constraint)
 
     # Add col constraints i.e. 11 != 21 != 31
     for col in range(1, N+1):
         constraint = Constraint("Col{}Con".format(col), [row[col-1] for row in variables])
+        constraint.add_satisfying_tuples(satisfying_tuples)
         constraints.append(constraint)
-
-    # Add satisfying tuples
-    for con in constraints:
-        satisfying_tuples = []
-        for tup in itertools.permutations(domain, N):
-            satisfying_tuples.append(tup)
-        con.add_satisfying_tuples(satisfying_tuples)
 
     # Flatten the 2-D variables matrix to a list
     variables = list(itertools.chain(*variables))
@@ -124,5 +127,104 @@ def nary_ad_grid(kenken_grid):
     return csp, variables
 
 def kenken_csp_model(kenken_grid):
-    # TODO! IMPLEMENT THIS!
-    pass
+    #pass
+    N = kenken_grid[0][0]
+    # Generate the domain
+    domain = list(range(1, N+1))
+
+    # Generate a matrix of variables
+    variables = []
+    for i in range(1, N+1):
+        var_row = []
+        for j in range(1, N+1):
+            var_row.append(Variable('Var{}{}'.format(i, j), domain))
+        variables.append(var_row)
+
+    constraints = []
+    # Operation constraints
+    for cageIdx in range(1, len(kenken_grid)):
+        cage = kenken_grid[cageIdx]
+        if len(cage) > 2:
+            operator = cage[-1]
+            target = cage[-2]
+            cageVariables = []
+            cageVariablesDomain = []
+            for cageVar in cage[:-2]:
+                i = cageVar//10
+                j = cageVar%10
+                cageVariables.append(variables[i-1][j-1])
+                cageVariablesDomain.append(domain)
+
+            constraint = Constraint('Cage{}'.format(cageIdx), cageVariables)
+
+            satisfying_tuples = []
+            for tup in itertools.product(*cageVariablesDomain):
+                # Addition
+                if operator == 0:
+                    if sum(tup) == target and tup not in satisfying_tuples:
+                        satisfying_tuples.append(tup)
+
+                # Subtraction
+                elif operator == 1:
+                    for perm in itertools.permutations(tup):
+                        diff = perm[0]
+                        for i in range(1, len(perm)):
+                            diff -= perm[i]
+                        if diff == target and tup not in satisfying_tuples:
+                            satisfying_tuples.append(tup)
+
+                # Division
+                elif operator == 2:
+                    for perm in itertools.permutations(tup):
+                        quotient = perm[0]
+                        for i in range(1, len(perm)):
+                            quotient //= perm[i]
+                        if quotient == target and tup not in satisfying_tuples:
+                            satisfying_tuples.append(tup)
+
+                # Multiplication
+                elif operator == 3:
+                    if functools.reduce(mul, tup) == target and tup not in satisfying_tuples:
+                        satisfying_tuples.append(tup)
+
+                constraint.add_satisfying_tuples(satisfying_tuples)
+                constraints.append(constraint)
+
+        else:       # len(cage) <= 2
+            # First element of cage = Variable
+            # Second element = value on the variable
+            target = cage[-1]
+            i = cage[0]//10
+            j = cage[0]%10
+            #cageVariable = variables[i-1][j-1]
+            #constraint = Constraint('CageVar{}{}'.format(i, j), cageVariable)
+            #constraint.add_satisfying_tuples((target))
+            #constraints.append(constraint)
+            variables[i-1][j-1] = Variable('V{}{}'.format(i, j), [target])
+
+
+    satisfying_tuples = []
+    for tup in itertools.permutations(domain, N):
+        satisfying_tuples.append(tup)
+
+    # Add row constraints i.e. 11 != 12 != 13
+    for row in range(1, N+1):
+        constraint = Constraint("Row{}Con".format(row), variables[row-1])
+        constraint.add_satisfying_tuples(satisfying_tuples)
+        constraints.append(constraint)
+
+    # Add col constraints i.e. 11 != 21 != 31
+    for col in range(1, N+1):
+        constraint = Constraint("Col{}Con".format(col), [row[col-1] for row in variables])
+        constraint.add_satisfying_tuples(satisfying_tuples)
+        constraints.append(constraint)
+
+    # Flatten the 2-D variables matrix to a list
+    #variables = list(itertools.chain(*variables))
+
+    # Create CSP
+    csp = CSP("KenKenModel", variables)
+    for con in constraints:
+        csp.add_constraint(con)
+
+    return csp, variables
